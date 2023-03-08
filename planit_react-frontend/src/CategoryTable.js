1ea7f0115b2db751f3e46c2b6b08261c985c9e86
juliayee <jye13@calpoly.edu>
2023-03-08 13:39:51 -0800

    category table (cat. hard coded)

---
+++ planit_react-frontend/src/CategoryTable.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function TableHeader()  {
+    return (
+      <thead>
+        <tr>
+          <th>Category</th>
+          <th>Due Date</th>
+          <th>Category</th>
+          <th>Remove</th>
+        </tr>
+      </thead>
+    );
+  }
+
+  function TableBody(props) {
+    const category_val = "csc 307";
+    const rows = props.characterData.map((row, index) => {
+      if(row.category === category_val){
+      return (
+        <tr key={index}>      
+  <td>{row.task}</td>
+  <td>{row.duedate}</td>
+  <td>{row.category}</td>
+  <td>
+    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+  </td>
+</tr>
+      );
+    } else{
+      return null;
+    }
+     }
+    );
+    return (
+        <tbody>
+          {rows}
+         </tbody>
+     );
+  }
+  
+  
+  function Table (props) {
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+      </table>
+    );
+  }
+  export default Table;
