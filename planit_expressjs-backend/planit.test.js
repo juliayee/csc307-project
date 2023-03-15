c3306297e630aa01097ea067b92d5b98fa55af1d
CanlinCao <ccao07@calpoly.edu>
2023-03-14 19:32:40 -0700

    made it pretty

--- planit_expressjs-backend/planit.test.js
+++ planit_expressjs-backend/planit.test.js
@@ -1,16 +1,19 @@
 // exports.findUserBydeadline = findUserBydeadline;
 // exports.updateList = updateList;
 
-import { findUserBycategory } from './backend.js';
+import { findUserBycategory } from "./backend.js";
 
-const myFunctions = require('./backend.js');
-const myFunctions = require('.MyApp.js');
+const myFunctions = require("./backend.js");
+const myFunctions = require(".MyApp.js");
 
-test('Testing findUserBydeadline -- success', () => {
-    const users = [["csc307", "project", "3/14/23", "yes"], ["ant101", "lab 4", "3/16/23", "no"]];
-    const target = ["csc307", "project", "3/14/23", "yes"];
-    const result = myFunctions.findUserBydeadline("3/14/23");
-    expect(target).toBe(result);
+test("Testing findUserBydeadline -- success", () => {
+  const users = [
+    ["csc307", "project", "3/14/23", "yes"],
+    ["ant101", "lab 4", "3/16/23", "no"],
+  ];
+  const target = ["csc307", "project", "3/14/23", "yes"];
+  const result = myFunctions.findUserBydeadline("3/14/23");
+  expect(target).toBe(result);
 });
 
 // test('Testing updateList -- success', () => {
@@ -20,4 +23,4 @@ test('Testing findUserBydeadline -- success', () => {
 //     expect(target).toBe(result);
 // });
 // doesnt return anything so prob a bad one to test
-// Can says compare the list after updating it 
+// Can says compare the list after updating it
