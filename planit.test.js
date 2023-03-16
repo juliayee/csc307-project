1dbcd2f2955c2ad8698563c115d12a78f95ce614
Madelyn Cruz <madelyn@Madelyns-Air.lan>
2023-03-16 03:54:05 -0700

    added 'asynch' to tests but parens problem

--- planit.test.js
+++ planit.test.js
@@ -20,44 +20,108 @@ deleteUserBydueDate
 deleteUserById
 */
 
-import { getUsers } from './planit_expressjs-backend/models/user-services.js';
-import './user-services.js';
+import { getUsers } from "./planit_expressjs-backend/models/user-services.js";
+import "./user-services.js";
 
-const myFunctions = require('./user-services.js');
+const myFunctions = require("./user-services.js");
 
-test('Testing getUsers 1, all params undefined -- success', () => {
-    const result = user-services.getUsers({});
-    expect(result).toEqual(expect.arrayContaining(result));
+test("Testing getUsers 1, all params undefined -- success", async () => {
+  const result = (await user) - services.getUsers({});
+  expect(result).toEqual(expect.arrayContaining(result));
 });
 
-test('Testing getUsers 2, only task undefined -- success', () => {
-    const result = user-services.getUsers({task: undefined, category: "math", duedate: 3/15/23, priority: "no"});
-    expect(result).toEqual(expect.arrayContaining(result));
+test("Testing getUsers 2, only task undefined -- success", async () => {
+  const result =
+    (await user) - // I don't want these parens but when I click save it adds them!!?
+    services.getUsers({
+      task: undefined,
+      category: "math",
+      duedate: 3 / 15 / 23,
+      priority: "no",
+    });
+  expect(result).toEqual(expect.arrayContaining(result));
 });
 
-test('Testing getUsers 3, only category undefined -- success', () => {
-    const result = user-services.getUsers({task: "project 1", category: undefined, duedate: 3/15/23, priority: "no"});
-    expect(result).toEqual(expect.arrayContaining(result));
+test("Testing getUsers 3, only category undefined -- success", async () => {
+  const result =
+    (await user) -
+    services.getUsers({
+      task: "project 1",
+      category: undefined,
+      duedate: 3 / 15 / 23,
+      priority: "no",
+    });
+  expect(result).toEqual(expect.arrayContaining(result));
 });
 
-test('Testing getUsers 4, only duedate undefined -- success', () => {
-    const result = user-services.getUsers({task: "project 1", category: "csc307", duedate: undefined, priority: "no"});
-    expect(result).toEqual(expect.arrayContaining(result));
+test("Testing getUsers 4, only duedate undefined -- success", async () => {
+  const result =
+    user -
+    services.getUsers({
+      task: "project 1",
+      category: "csc307",
+      duedate: undefined,
+      priority: "no",
+    });
+  expect(result).toEqual(expect.arrayContaining(result));
 });
 
-test('Testing getUsers 5, only priority undefined -- success', () => {
-    const result = user-services.getUsers({task: "project 1", category: "csc307", duedate: 3/15/23, priority: undefined});
-    expect(result).toEqual(expect.arrayContaining(result));
+test("Testing getUsers 5, only priority undefined -- success", async () => {
+  const result =
+    user -
+    services.getUsers({
+      task: "project 1",
+      category: "csc307",
+      duedate: 3 / 15 / 23,
+      priority: undefined,
+    });
+  expect(result).toEqual(expect.arrayContaining(result));
 });
 
-test('Testing deleteUser1 -- success', () => {
-    const delUser = user-services.deleteUsers({});
-    expect(result).toEqual(false);
+test("Testing deleteUser1, all undefined -- success", async () => {
+  const delUser = user - services.deleteUsers({});
+  expect(result).toEqual(false);
 });
 
-test('Testing findUserBydueDate -- success', () => {
-    const users = [["csc307", "project", "3/14/23", "yes"], ["ant101", "lab 4", "3/16/23", "no"]];
-    const target = ["csc307", "project", "3/14/23", "yes"];
-    const result = myFunctions.findUserBydueDate("3/14/23");
-    expect(target).toBe(result);
+test("Testing deleteUser2, task undefined -- success", async () => {
+  const delUser =
+    user -
+    services.deleteUsers({
+      task: undefined,
+      category: "csc307",
+      duedate: 3 / 17 / 23,
+    });
+  expect(result).toEqual(false);
+});
+
+test("Testing deleteUser3, category undefined -- success", async () => {
+  const delUser =
+    user -
+    services.deleteUsers({
+      task: "make presentation",
+      category: undefined,
+      duedate: 3 / 17 / 23,
+    });
+  expect(result).toEqual(false);
+});
+
+test("Testing deleteUser4, duedate undefined -- success", async () => {
+  const delUser =
+    user -
+    services.deleteUsers({
+      task: "make presentation",
+      category: "math 144",
+      duedate: undefined,
+    });
+  expect(result).toEqual(false);
+});
+
+test("Testing findUserBydueDate -- success", async () => {
+  const users = [
+    ["csc307", "project", "3/14/23", "yes"],
+    ["ant101", "lab 4", "3/16/23", "no"],
+  ];
+  const target = ["csc307", "project", "3/14/23", "yes"];
+  const result = myFunctions.findUserBydueDate("3/14/23");
+  expect(target).toBe(result);
 });
