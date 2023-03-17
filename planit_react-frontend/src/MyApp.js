551b5b130fdbbd2c9a3f896ae9a6f43b4e94d723
CanlinCao <63250510+CanlinCao@users.noreply.github.com>
2023-03-17 13:50:32 -0700

    fixing delete

--- planit_react-frontend/src/MyApp.js
+++ planit_react-frontend/src/MyApp.js
@@ -34,7 +34,7 @@ function MyApp() {
 
   function removeOneCharacter(index) {
     // go to index, get the id, assign to id
-    const id = characters[index].id;
+    const id = characters[index]._id;
     console.log(id);
     axios.delete('http://localhost:5000/users/'+id);         
     const updated = characters.filter((character, i) => {
